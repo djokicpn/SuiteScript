918b99f5dffa40a839b4ad12c65314e207abbf40
trungpv1601 <trungpv1601@gmail.com>
2019-07-31 14:22:00 +0700

    Update Customer Deposite

---
+++ Features/Customer Deposit/customer_deposit_form_ues.js
@@ -0,0 +1,33 @@
+/**
+ * Customer Deposit Form User Event Script
+ * custform_248_4283482_820
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @author trungpv <trung@lexor.com>
+ */
+define(['./Module/salesEffective'], function(salesEffective) {
+	function beforeLoad(context) {
+		var form = context.form;
+		// Set Under. Funds as Default
+		var undepfunds = form.getField({ id: 'undepfunds' });
+		if (undepfunds) {
+			undepfunds.defaultValue = 'T';
+		}
+	}
+
+	function beforeSubmit(context) {}
+
+	function afterSubmit(context) {
+		var newRecord = context.newRecord;
+		var newRecordSaleOrder = newRecord.getValue({
+			fieldId: 'salesorder'
+		});
+		salesEffective.update(newRecordSaleOrder);
+	}
+
+	return {
+		beforeLoad: beforeLoad,
+		beforeSubmit: beforeSubmit,
+		afterSubmit: afterSubmit
+	};
+});
