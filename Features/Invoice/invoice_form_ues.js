8c151a7defbd8da644fdae10fc0364930c5b3b13
trungpv <trungpv1601@gmail.com>
2019-09-04 10:37:33 +0700

    Remove https://trello.com/c/4YxMBpAQ/161-set-line-margin-balance-value-after-so-status-change-to-billed

--- Features/Invoice/invoice_form_ues.js
+++ Features/Invoice/invoice_form_ues.js
@@ -6,53 +6,40 @@
  * @NScriptType UserEventScript
  * @author trungpv <trung@lexor.com>
  */
-define(["N/record", "./Module/marginBalance"], function(record, marginBalance) {
-    function beforeLoad(context) {
-        const type = context.type;
-        if (type !== context.UserEventType.CREATE) return;
-        var invoiceRecord = context.newRecord;
-        if (invoiceRecord.type === "invoice") {
-            try {
-                // saleseffectivedate
-                const saleOrderId = invoiceRecord.getValue("createdfrom");
-                if (saleOrderId) {
-                    var so = record.load({
-                        type: record.Type.SALES_ORDER,
-                        id: saleOrderId,
-                        isDynamic: true
-                    });
-                    const saleseffectivedate = so.getValue("saleseffectivedate");
-                    invoiceRecord.setValue({
-                        fieldId: "saleseffectivedate",
-                        value: saleseffectivedate
-                    });
-                }
-            } catch (error) {
-                log.error({
-                    title: "---- Error beforeLoad Invoice " + invoiceRecord.id,
-                    details: error.message
-                });
-            }
-        }
-    }
+define(['N/record'], function(record) {
+	function beforeLoad(context) {
+		const type = context.type;
+		if (type !== context.UserEventType.CREATE) return;
+		var invoiceRecord = context.newRecord;
+		if (invoiceRecord.type === 'invoice') {
+			try {
+				// saleseffectivedate
+				const saleOrderId = invoiceRecord.getValue('createdfrom');
+				if (saleOrderId) {
+					var so = record.load({
+						type: record.Type.SALES_ORDER,
+						id: saleOrderId,
+						isDynamic: true
+					});
+					const saleseffectivedate = so.getValue('saleseffectivedate');
+					invoiceRecord.setValue({
+						fieldId: 'saleseffectivedate',
+						value: saleseffectivedate
+					});
+				}
+			} catch (error) {
+				log.error({
+					title: '---- Error beforeLoad Invoice ' + invoiceRecord.id,
+					details: error.message
+				});
+			}
+		}
+	}
 
-    function afterSubmit(context) {
-        var invoiceRecord = context.newRecord;
-        if (invoiceRecord.type === "invoice") {
-            try {
-				const saleOrderId = invoiceRecord.getValue("createdfrom");
-				marginBalance.updateSO(saleOrderId);
-            } catch (error) {
-                log.error({
-                    title: "---- Error afterSubmit Invoice " + invoiceRecord.id,
-                    details: error.message
-                });
-            }
-        }
-    }
+	function afterSubmit(context) {}
 
-    return {
-        beforeLoad: beforeLoad,
-        afterSubmit: afterSubmit
-    };
+	return {
+		beforeLoad: beforeLoad,
+		afterSubmit: afterSubmit
+	};
 });
