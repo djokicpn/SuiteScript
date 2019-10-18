bf886bb9c03de304ecde00d05e0b974c41be4d57
trungpv <trungpv1601@gmail.com>
2019-10-18 09:54:35 +0700

    https://trello.com/c/ONi1C60E/213-se-approval

--- Module/salesEffective.js
+++ Module/salesEffective.js
@@ -15,89 +15,96 @@ define(['N/record', 'N/search'], function(record, search) {
 					id: salesOrderId,
 					isDynamic: true
 				});
-				const createdDate = so.getValue('createddate');
-				const total = parseFloat(so.getValue('total'));
-				if (total > 0) {
-					// https://trello.com/c/8kvQ8u6S/200-check-boxes-customer-deposit-0-and-sale-effective-day
-					// 18 : Credit Memo
-					// 7 : Prepaid
-					const terms = so.getValue('terms');
-					if (terms !== '7') {
-						const isSaleEffective = so.getValue('custbody_sales_effective_checked');
-						so.setValue({
-							fieldId: 'custbody_cd_greater_than_zero',
-							value: true
-						});
-						if (!isSaleEffective) {
-							so.setValue({
-								fieldId: 'saleseffectivedate',
-								value: new Date()
-							});
-						}
-						so.setValue({
-							fieldId: 'custbody_sales_effective_checked',
-							value: true
-						});
-					} else {
-						var listDeposits = search.create({
-							type: search.Type.CUSTOMER_DEPOSIT,
-							filters: [
-								{
-									name: 'salesorder',
-									operator: search.Operator.IS,
-									values: [salesOrderId]
+				if (so) {
+					const createdDate = so.getValue('createddate');
+					const total = parseFloat(so.getValue('total'));
+					const isSEApproved = so.getValue('custbody_se_approved');
+					if (!isSEApproved) {
+						if (total > 0) {
+							// https://trello.com/c/8kvQ8u6S/200-check-boxes-customer-deposit-0-and-sale-effective-day
+							// 18 : Credit Memo
+							// 7 : Prepaid
+							const terms = so.getValue('terms');
+							if (terms !== '7') {
+								const isSaleEffective = so.getValue('custbody_sales_effective_checked');
+								so.setValue({
+									fieldId: 'custbody_cd_greater_than_zero',
+									value: true
+								});
+								if (!isSaleEffective) {
+									so.setValue({
+										fieldId: 'saleseffectivedate',
+										value: new Date()
+									});
 								}
-							],
-							columns: [
-								search.createColumn({
-									name: 'trandate',
-									sort: search.Sort.ASC
-								}),
-								'amount',
-								'custbody_date_deposited',
-								'status',
-								'custbody65',
-								'custbody66'
-							]
-						});
-						var totalDeposited = 0;
-						var salesEffectiveDate = false;
-						var customerDepositGreaterThanZero = false;
-						listDeposits.run().each(function(item) {
-							var status = item.getValue('status');
-							var trandate = item.getValue('trandate');
-							var custbody_date_deposited = item.getValue('custbody_date_deposited');
-							var paymentReceived = item.getValue('custbody65');
-							var dateReceived = item.getValue('custbody66');
-							if (paymentReceived && status !== 'unapprovedPayment' && !isRefund(item.id)) {
-								var totalValue = item.getValue('amount');
-								totalDeposited = parseFloat(totalDeposited) + parseFloat(totalValue);
-								if (totalDeposited > 0) {
-									customerDepositGreaterThanZero = true;
-								}
-								var totalDepositedPercent = (totalDeposited / total) * 100;
-								if (totalDepositedPercent >= SALE_EFFECTIVE_RATE) {
-									salesEffectiveDate = dateReceived ? new Date(dateReceived) : new Date(trandate);
-									return false;
-								}
+								so.setValue({
+									fieldId: 'custbody_sales_effective_checked',
+									value: true
+								});
+							} else {
+								var listDeposits = search.create({
+									type: search.Type.CUSTOMER_DEPOSIT,
+									filters: [
+										{
+											name: 'salesorder',
+											operator: search.Operator.IS,
+											values: [salesOrderId]
+										}
+									],
+									columns: [
+										search.createColumn({
+											name: 'trandate',
+											sort: search.Sort.ASC
+										}),
+										'amount',
+										'custbody_date_deposited',
+										'status',
+										'custbody65',
+										'custbody66'
+									]
+								});
+								var totalDeposited = 0;
+								var salesEffectiveDate = false;
+								var customerDepositGreaterThanZero = false;
+								listDeposits.run().each(function(item) {
+									var status = item.getValue('status');
+									var trandate = item.getValue('trandate');
+									var custbody_date_deposited = item.getValue('custbody_date_deposited');
+									var paymentReceived = item.getValue('custbody65');
+									var dateReceived = item.getValue('custbody66');
+									if (paymentReceived && status !== 'unapprovedPayment' && !isRefund(item.id)) {
+										var totalValue = item.getValue('amount');
+										totalDeposited = parseFloat(totalDeposited) + parseFloat(totalValue);
+										if (totalDeposited > 0) {
+											customerDepositGreaterThanZero = true;
+										}
+										var totalDepositedPercent = (totalDeposited / total) * 100;
+										if (totalDepositedPercent >= SALE_EFFECTIVE_RATE) {
+											salesEffectiveDate = dateReceived
+												? new Date(dateReceived)
+												: new Date(trandate);
+											return false;
+										}
+									}
+									return true;
+								});
+								so.setValue({
+									fieldId: 'custbody_cd_greater_than_zero',
+									value: customerDepositGreaterThanZero
+								});
+								so.setValue({
+									fieldId: 'custbody_sales_effective_checked',
+									value: salesEffectiveDate ? true : false
+								});
+								so.setValue({
+									fieldId: 'saleseffectivedate',
+									value: salesEffectiveDate ? salesEffectiveDate : new Date(createdDate)
+								});
 							}
-							return true;
-						});
-						so.setValue({
-							fieldId: 'custbody_cd_greater_than_zero',
-							value: customerDepositGreaterThanZero
-						});
-						so.setValue({
-							fieldId: 'custbody_sales_effective_checked',
-							value: salesEffectiveDate ? true : false
-						});
-						so.setValue({
-							fieldId: 'saleseffectivedate',
-							value: salesEffectiveDate ? salesEffectiveDate : new Date(createdDate)
-						});
-					}
 
-					so.save();
+							so.save();
+						}
+					}
 				}
 			} catch (err) {
 				log.error({
